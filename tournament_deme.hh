5ad84e9047060a776d4ad0af10ffb388eecc98b6
vagrant <vagrant@ubuntu>
2018-11-29 17:42:26 +0000

    wrote tournament_deme

---
+++ tournament_deme.hh
@@ -0,0 +1,12 @@
+#pragama once
+#include "chromosome.hh"
+#include "cities.hh"
+#include <random>
+#include "deme.hh"
+
+class TournamentDeme: public Deme {
+    TournamentDeme(const Cities* cities_ptr, unsigned pop_size, double mut_rate, int tournament_size);
+    virtual Chromosome* select_parent() override;
+    protected:
+        int tournament_size_
+}
